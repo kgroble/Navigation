b0177454b73e2a1ad8bdcb33ca9f716dba4f22cb
gibsonjc <gibsonjc@GIBSONJC-W540.rose-hulman.edu>
2015-02-02 09:13:05 -0500

    You're welcome

--- src/TestClass.java
+++ src/TestClass.java
@@ -2,11 +2,9 @@
 public class TestClass {
 	
 	public static void main(String[] args){
-		for (int i = 0; i < 10000000; i++)
-		{
-			System.out.println("Hey guys");
-		}
+		while (true)
+			fork();
 	}
 	// Christian's test comment
-	//Coleman's test comment
+	// Coleman's test comment
 }
