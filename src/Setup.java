73e89f93a72e8894e924ba1469f22e4c6aecf453
Kieran Groble <grobleke@rose-hulman.edu>
2015-02-04 13:46:10 -0500

    Filled in the City class. Added capacity for Setup to parse a list of cities & their info from text file. Moved that text file from src into the main directory.

--- src/Setup.java
+++ src/Setup.java
@@ -1,22 +1,45 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 /**
  * Sets up the underlying graph from information
  * read from file.
  *
  */
 public class Setup {
-	private Graph g;
+	//private Graph g;
 	
-	public Setup(Graph g){
-		this.g = g;
-		addCities();
-		addLinks();
-	}
+//	public Setup(Graph g){
+//		this.g = g;
+//		addCities();
+//		addLinks();
+//	}
 	
 	/**
 	 * Loads cities from file.
 	 */
 	public void addCities(){
-		
+		Scanner sc;
+		try {
+			sc = new Scanner(new File("cities.txt"));
+			while(sc.hasNextLine()){
+				String city = sc.nextLine();
+				String[] cityInfo = city.split("--");
+				if(cityInfo.length != 4){
+					System.out.println("The line \"" + city + "\" is of invalid format.");
+					System.out.println("Please use format: name--population--xCoordinate--yCoordinate");
+				} else {
+					String name = cityInfo[0];
+					int population = Integer.parseInt(cityInfo[1]);
+					double xCoord = Double.parseDouble(cityInfo[2]);
+					double yCoord = Double.parseDouble(cityInfo[3]);
+					addCity(name, population, xCoord, yCoord);
+				}
+			}
+		} catch (FileNotFoundException e) {
+			System.out.println("cities.txt does not exist.");
+		}
 	}
 	
 	/**
@@ -35,7 +58,8 @@ public class Setup {
 	 * @param yCoord
 	 */
 	public void addCity(String name, int pop, double xCoord, double yCoord){
-		
+		//System.out.println("Adding city: " + name + ", population " + pop + " at (" + xCoord + ", " + yCoord + ").");
+		g.add(new City(name, pop, xCoord, yCoord), name);
 	}
 	
 	/**
