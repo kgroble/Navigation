dce5fe716ef7ed2fd9f9a17ecfd250044bab8575
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2015-02-04 14:12:55 -0500

    Added MapPanel

---
+++ src/MapPanel.java
@@ -0,0 +1,17 @@
+import javax.swing.JPanel;
+
+/**
+ * 
+ * A panel that will visually represent the data contained in a graph object
+ * 
+ * @author nunnalcs
+ *
+ */
+public class MapPanel extends JPanel {
+
+	private static final long serialVersionUID = 1L;
+
+	public MapPanel(Graph<City, Connection, String> map) {
+
+	}
+}
