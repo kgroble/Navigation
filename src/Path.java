249d6ea6bf844c6896ea8dd82cc79cc699a7d2c9
gibsonjc <gibsonjc@GIBSONJC-W540.rose-hulman.edu>
2015-02-04 13:33:49 -0500

    Connection and a couple other things added

--- src/Path.java
+++ src/Path.java
@@ -1,6 +1,9 @@
+import java.util.ArrayList;
+
 
 public class Path<T> implements Comparable<T>
 {
+	private ArrayList<City> path;
 	private double pathLength;
 	//for comparing to other paths, for use in the priority queue
 	//base off distance, use another form of ranking to pick after 
@@ -19,5 +22,16 @@ public class Path<T> implements Comparable<T>
 	public double getPathLength()
 	{
 		return this.pathLength;
+	}
+	
+	/**
+	 * We will probably need to add some heuristic stuff here.
+	 * @param link
+	 * @param city
+	 */
+	public void addToPath(Connection link, City city)
+	{
+		this.path.add(city);
+		this.pathLength += link.getConnectionDistance();
 	}
 }
