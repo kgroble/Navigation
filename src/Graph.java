90f3571c923d272ec741194ea0b048921473793c
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2015-02-04 13:47:46 -0500

    Graph add and addLink should work now

--- src/Graph.java
+++ src/Graph.java
@@ -2,60 +2,80 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 
-
-public class Graph<T extends Comparable<? super T>, K> implements
-		Iterable<Graph.Node> {
+public class Graph<T, L, K> implements Iterable<Graph.Node> {
 
 	HashMap<K, Node> nodes;
-	
-	public Graph()
-	{
+
+	public Graph() {
 		nodes = new HashMap<K, Node>();
 	}
-	
-	public boolean add(T element, K key)
-	{
+
+	/**
+	 * Inserts the element into the Graph as an unlinked node
+	 * 
+	 * @param element
+	 *            The element of type T to add
+	 * @param key
+	 *            Used as a key/value pair
+	 * @return True is the element was inserted
+	 */
+	public boolean add(T element, K key) {
+		if (nodes.containsKey(key))
+			return false;
+		Node node = new Node(element);
+		nodes.put(key, node);
+		return true;
+	}
+
+	/**
+	 * Adds a one way connection between two nodes
+	 * 
+	 * @param from
+	 *            the key of the object the link is from
+	 * @param to
+	 *            the key of the object the link is to
+	 * @param connection
+	 *            the link object to be stored with this link
+	 * @return True if the link is added
+	 */
+	public boolean addLink(K from, K to, L connection) {
+		if (nodes.containsKey(from) && nodes.containsKey(to)) {
+			Link link = new Link(nodes.get(to), connection);
+			nodes.get(from).addLink(link);
+		}
+		return false;
+	}
+
+	public boolean remove(T element) {
 		throw new UnsupportedOperationException();
 	}
-	
-	public boolean addLink(K from, K to)
-	{
+
+	public boolean removeLink(K from, K to) {
 		throw new UnsupportedOperationException();
 	}
-	
-	public boolean remove(T element)
-	{
-		throw new UnsupportedOperationException();
-	}
-	
-	public boolean removeLink(K from, K to)
-	{
-		throw new UnsupportedOperationException();
-	}
-	
-	
-	public class Node
-	{
+
+	public class Node {
 		ArrayList<Link> links;
 		T element;
-		
-		public Node()
-		{
+
+		public Node(T element) {
 			links = new ArrayList();
+			this.element = element;
 		}
-		
-		private boolean addLink(Link link){
+
+		private boolean addLink(Link link) {
 			links.add(link);
 			throw new UnsupportedOperationException();
 		}
 	}
-	
-	public class Link
-	{
+
+	public class Link {
 		Node connection;
-		public Link(Node connection)
-		{
+		L link;
+
+		public Link(Node connection, L link) {
 			this.connection = connection;
+			this.link = link;
 		}
 	}
 
