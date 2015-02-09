c9b81d9f1a8f31d32b5ac7fbcdb991356d6f629f
Adam Finer <fineral@rose-hulman.edu>
2015-02-09 09:44:32 -0500

    UPDATE application classes

--- src/GUIMain.java
+++ src/GUIMain.java
@@ -12,35 +12,10 @@ import javax.swing.JTextArea;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.TitledBorder;
 
-
-
-
 public class GUIMain {
 	
-	private static final int FRAME_WIDTH=1000;
-	private static final int FRAME_HEIGHT=600;
-	
 	public static void main(String[] args){
-		JFrame myFrame = new JFrame();
-		
-		//make container
-		Container contentContainer = myFrame.getContentPane();
-		BorderLayout jBorderLayout = new BorderLayout();
-		
-		//
-		MyPanel j = new MyPanel(0);
-		contentContainer.add(j,jBorderLayout.CENTER);
-		
-		MapPanel i = new MapPanel(new Graph<City,Connection,String>());
-		i.setBounds(0, (int)(FRAME_HEIGHT*.1), FRAME_WIDTH,FRAME_HEIGHT-(int)(FRAME_HEIGHT*.1));
-		contentContainer.add(i,jBorderLayout.SOUTH);
-		
-		myFrame.setLayout(jBorderLayout);
-		j.setBounds(0, 0, FRAME_WIDTH, (int)(FRAME_HEIGHT*.1));
-		
-		myFrame.setSize(FRAME_WIDTH,FRAME_HEIGHT);
-		myFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		myFrame.setVisible(true);
+		new Application();
 	}
 
 	
