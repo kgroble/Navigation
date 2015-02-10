9e8a7aac56b1feb5bf957f709a2af0caf02812a4
Adam Finer <fineral@rose-hulman.edu>
2015-02-10 15:12:36 -0500

    minimum box size created

--- src/ApplicationWindow.java
+++ src/ApplicationWindow.java
@@ -1,5 +1,6 @@
 import java.awt.BorderLayout;
 import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ComponentAdapter;
@@ -12,40 +13,53 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.JTextPane;
 import javax.swing.border.BevelBorder;
 import javax.swing.border.TitledBorder;
 
 public class ApplicationWindow extends JFrame {
 
-	private static int FRAME_WIDTH=1000;
-	private static int FRAME_HEIGHT=600;
+	private static final int FRAME_WIDTH=1000;
+	private static final int FRAME_HEIGHT=600;
 	private MapPanel mapPanel;
 	private MyPanel myPanel;
 	
 	public ApplicationWindow(Graph<City,Connection,String> map){
+		//Create container and establish border layout
 		Container contentContainer = getContentPane();
 		BorderLayout jBorderLayout = new BorderLayout();
+		this.setLayout(jBorderLayout);
 		
+		//create and add myPanel
 		myPanel = new MyPanel(0);
 		contentContainer.add(myPanel,jBorderLayout.CENTER);
+		myPanel.setBounds(0, 0, FRAME_WIDTH, 75);
 		
+		//create and add mapPanel
 		mapPanel = new MapPanel(map);
 		mapPanel.setBounds(0, 75, FRAME_WIDTH,FRAME_HEIGHT-75);
 		contentContainer.add(mapPanel,jBorderLayout.SOUTH);
 		
-		setLayout(jBorderLayout);
-		myPanel.setBounds(0, 0, FRAME_WIDTH, 75);
-		
 		setSize(FRAME_WIDTH,FRAME_HEIGHT);
+		setMinimumSize(new Dimension(FRAME_WIDTH, FRAME_HEIGHT));
 		
 		this.addComponentListener(new ComponentAdapter() {
 
             @Override
             public void componentResized(ComponentEvent e) {
-            	FRAME_WIDTH=getWidth();
-            	FRAME_HEIGHT=getHeight();
-            	myPanel.setBounds(0, 0, FRAME_WIDTH, 75);
-            	mapPanel.setBounds(0, 75, FRAME_WIDTH,FRAME_HEIGHT-75);
+            	//retrieve new height and width info
+            	int newWidth=getWidth();
+            	int newHeight=getHeight();
+            	
+            	//manipulate the panels
+            	if(newWidth>=FRAME_WIDTH){
+            		myPanel.setBounds(0, 0, newWidth, 75);
+            		mapPanel.setBounds(0,75,newWidth,mapPanel.getHeight());
+            	}
+            	if(newHeight>=FRAME_HEIGHT)
+        			mapPanel.setBounds(0, 75, mapPanel.getWidth(),newHeight-75);
+            	
+            	//update the frame
             	repaint();
             }
         });
@@ -55,10 +69,11 @@ public class ApplicationWindow extends JFrame {
 	}
 	
 	public class MyPanel extends JComponent {
+		private static final long serialVersionUID = 7088760637095647696L;
 		private JButton enter;
 		private JTextArea from, to;
 		private JLabel toLabel, fromLabel, distanceLabel, timeLabel;
-		private JTextField distance, time;
+		private JTextPane distance, time;
 		private final static int MARGIN = 25;
 
 		public MyPanel(int height) {
@@ -69,7 +84,7 @@ public class ApplicationWindow extends JFrame {
 			border.setTitleJustification(TitledBorder.LEFT);
 			this.setBorder(border);
 			
-			distance=new JTextField();
+			distance=new JTextPane();
 			distance.setBorder(new BevelBorder(1));
 			distance.setBounds(850,height+MARGIN,100,20);
 			this.add(distance);
@@ -79,7 +94,7 @@ public class ApplicationWindow extends JFrame {
 			distanceLabel.setBounds(765,height+MARGIN,100,20);
 			this.add(distanceLabel);
 			
-			time = new JTextField();
+			time = new JTextPane();
 			time.setBorder(new BevelBorder(1));
 			time.setBounds(650,height+MARGIN,100,20);
 			this.add(time);
@@ -114,11 +129,14 @@ public class ApplicationWindow extends JFrame {
 
 				@Override
 				public void actionPerformed(ActionEvent e) {
+					//Take input from the user
 					String toS = to.getText();
 					String fromS = from.getText();
 					
+					//send input to the map panel and receive informative string
 					String a=mapPanel.fromTo(toS, fromS);
 					
+					//display output
 					distance.setText(toS);
 					time.setText(fromS);
 				}
