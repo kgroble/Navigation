f4d6e9280fe5f594cd1068e842c1eac87dd10fab
Christian-Nunnally <nunnalcs@rose-hulman.edu>
2015-02-16 09:33:12 -0500

    Fixed bugs, removed setupTest

--- src/AStarTest.java
+++ src/AStarTest.java
@@ -4,110 +4,116 @@ import java.util.ArrayList;
 
 import org.junit.Test;
 
-public class AStarTest
-{
-	
-////	@Test
-//	public void test()
-//	{
-//		Graph<City, Connection, String> graph = new Graph<City, Connection, String>();
-//		AStar pathfinder = new AStar(graph);
-//		ArrayList<City> cities = new ArrayList<City>();
-//		assertTrue(cities.add(new City("Detroit", 0, 0, 0, 0)));
-//		assertTrue(cities.add(new City("Chicago", 0, 0, 1, 1)));
-//		assertTrue(cities.add(new City("New York", 0, 0, 1, 2)));
-//		
-//		for (City city : cities)
-//		{
-//			assertTrue(graph.add(city, city.getName()));
-//		}
-//		
-//		assertTrue(graph.addLink(cities.get(0).getName(), cities.get(1).getName(), 
-//				new Connection(cities.get(0).distanceTo(cities.get(1)))));
-//		assertTrue(graph.addLink(cities.get(0).getName(), cities.get(2).getName(), 
-//				new Connection(cities.get(0).distanceTo(cities.get(2)))));
-//		assertTrue(graph.addLink(cities.get(1).getName(), cities.get(2).getName(), 
-//				new Connection(cities.get(1).distanceTo(cities.get(2)))));
-//		
-//		String path = pathfinder.findShortestPathBetween(
-//				cities.get(0).getName(), cities.get(1).getName()).toString();
-//		
-//		System.out.println(path);
-//		assertTrue(path.equals("[Detroit, Chicago]"));
-//	}
-//	
-////	@Test
-//	public void legitTest()
-//	{
-//		Graph<City,Connection,String> map = new Graph<City,Connection,String>();
-//		Setup setup = new Setup(map);
-//		AStar pathfinder = new AStar(map);
-//		
-//		Path path = pathfinder.findShortestPathBetween("Arad", "Bucharest");
-//		assertTrue(path.toString().equals("[Arad, Sibiu, Fagaras, Bucharest]"));
-//	}	
-//	
-////	@Test
-//	public void testTravelDistanceSearch()
-//	{
-//		Graph<City,Connection,String> map = new Graph<City,Connection,String>();
-//		Setup setup = new Setup(map);
-//		AStar pathfinder = new AStar(map);
-//		
-//		double len = pathfinder.findShortestPathBetween("Arad", "Zerind").getPathLength();
-////		ArrayList<Path> paths = pathfinder.findPathsWithTravelDistance("Arad", len-1, len+1);
-////		System.out.println(paths.size());
-////		for (Path path : paths)
-////			System.out.println(path);
-////		assertTrue(paths.size()==1);
-//		
-//	}
-//	
-////	@Test
-//	public void testTravelTimeSearch()
-//	{
-//		Graph<City,Connection,String> map = new Graph<City,Connection,String>();
-//		Setup setup = new Setup(map);
-//		AStar pathfinder = new AStar(map);
-//		
-//		double len = pathfinder.findShortestPathBetween("Arad", "Zerind").getPathTravelTime();
-////		ArrayList<Path> paths = pathfinder.findPathsWithTravelTime("Arad", len-10, len+10);
-//		
-////		assertTrue(paths.size()==1);
-//		
-//	}
-//	
-//	@Test
-//	public void testFindFastestRoute()
-//	{
-//		Graph<City,Connection,String> map = new Graph<City,Connection,String>();
-//		Setup setup = new Setup(map);
-//		AStar pathfinder = new AStar(map);
-//		
-//		Path path = pathfinder.findFastestPath("Craiova", "Pitesti");
-//		System.out.println(path);
-//		assertTrue(path.getCities().size() == 3);
-//		
-//	}
-//	
-//	@Test
-//	public void testFindFastestRoute1()
-//	{
-//		Graph<City,Connection,String> map = new Graph<City,Connection,String>();
-//		Setup setup = new Setup(map);
-//		AStar pathfinder = new AStar(map);
-//		
-//		Path path1 = pathfinder.findFastestPath("Craiova", "Pitesti");
-//		System.out.println(path1);
-//		assertTrue(path1.getCities().size() == 3);
-//		
-//	}
-	
+public class AStarTest {
+
+	// // @Test
+	// public void test()
+	// {
+	// Graph<City, Connection, String> graph = new Graph<City, Connection,
+	// String>();
+	// AStar pathfinder = new AStar(graph);
+	// ArrayList<City> cities = new ArrayList<City>();
+	// assertTrue(cities.add(new City("Detroit", 0, 0, 0, 0)));
+	// assertTrue(cities.add(new City("Chicago", 0, 0, 1, 1)));
+	// assertTrue(cities.add(new City("New York", 0, 0, 1, 2)));
+	//
+	// for (City city : cities)
+	// {
+	// assertTrue(graph.add(city, city.getName()));
+	// }
+	//
+	// assertTrue(graph.addLink(cities.get(0).getName(),
+	// cities.get(1).getName(),
+	// new Connection(cities.get(0).distanceTo(cities.get(1)))));
+	// assertTrue(graph.addLink(cities.get(0).getName(),
+	// cities.get(2).getName(),
+	// new Connection(cities.get(0).distanceTo(cities.get(2)))));
+	// assertTrue(graph.addLink(cities.get(1).getName(),
+	// cities.get(2).getName(),
+	// new Connection(cities.get(1).distanceTo(cities.get(2)))));
+	//
+	// String path = pathfinder.findShortestPathBetween(
+	// cities.get(0).getName(), cities.get(1).getName()).toString();
+	//
+	// System.out.println(path);
+	// assertTrue(path.equals("[Detroit, Chicago]"));
+	// }
+	//
+	// // @Test
+	// public void legitTest()
+	// {
+	// Graph<City,Connection,String> map = new Graph<City,Connection,String>();
+	// Setup setup = new Setup(map);
+	// AStar pathfinder = new AStar(map);
+	//
+	// Path path = pathfinder.findShortestPathBetween("Arad", "Bucharest");
+	// assertTrue(path.toString().equals("[Arad, Sibiu, Fagaras, Bucharest]"));
+	// }
+	//
+	// // @Test
+	// public void testTravelDistanceSearch()
+	// {
+	// Graph<City,Connection,String> map = new Graph<City,Connection,String>();
+	// Setup setup = new Setup(map);
+	// AStar pathfinder = new AStar(map);
+	//
+	// double len = pathfinder.findShortestPathBetween("Arad",
+	// "Zerind").getPathLength();
+	// // ArrayList<Path> paths = pathfinder.findPathsWithTravelDistance("Arad",
+	// len-1, len+1);
+	// // System.out.println(paths.size());
+	// // for (Path path : paths)
+	// // System.out.println(path);
+	// // assertTrue(paths.size()==1);
+	//
+	// }
+	//
+	// // @Test
+	// public void testTravelTimeSearch()
+	// {
+	// Graph<City,Connection,String> map = new Graph<City,Connection,String>();
+	// Setup setup = new Setup(map);
+	// AStar pathfinder = new AStar(map);
+	//
+	// double len = pathfinder.findShortestPathBetween("Arad",
+	// "Zerind").getPathTravelTime();
+	// // ArrayList<Path> paths = pathfinder.findPathsWithTravelTime("Arad",
+	// len-10, len+10);
+	//
+	// // assertTrue(paths.size()==1);
+	//
+	// }
+	//
+	// @Test
+	// public void testFindFastestRoute()
+	// {
+	// Graph<City,Connection,String> map = new Graph<City,Connection,String>();
+	// Setup setup = new Setup(map);
+	// AStar pathfinder = new AStar(map);
+	//
+	// Path path = pathfinder.findFastestPath("Craiova", "Pitesti");
+	// System.out.println(path);
+	// assertTrue(path.getCities().size() == 3);
+	//
+	// }
+	//
+	// @Test
+	// public void testFindFastestRoute1()
+	// {
+	// Graph<City,Connection,String> map = new Graph<City,Connection,String>();
+	// Setup setup = new Setup(map);
+	// AStar pathfinder = new AStar(map);
+	//
+	// Path path1 = pathfinder.findFastestPath("Craiova", "Pitesti");
+	// System.out.println(path1);
+	// assertTrue(path1.getCities().size() == 3);
+	//
+	// }
+
 	@Test
-	public void stressTest()
-	{
-		Graph<City,Connection,String> map = new Graph<City,Connection,String>();
-		Setup setup = new Setup(map);
+	public void stressTest() {
+		Graph<City, Connection, String> map = new Graph<City, Connection, String>();
+		Setup setup = new Setup(map, "ireland");
 		AStar pathfinder = new AStar(map);
 		City start;
 		City end;
@@ -116,20 +122,21 @@ public class AStarTest
 		int startIndex;
 		int endIndex;
 		Random rand = new Random();
-		
+
 		ArrayList<City> elements = map.getElements();
-		
-		for (int i = 0; i < 100; i++)
-		{
+
+		for (int i = 0; i < 100; i++) {
 			startIndex = rand.nextInt(150);
 			endIndex = rand.nextInt(150);
-			
+
 			start = elements.get(startIndex);
 			end = elements.get(endIndex);
-			
-			distPath = pathfinder.findShortestPathBetween(start.getName(), end.getName());
-			timePath = pathfinder.findFastestPath(start.getName(), end.getName());
-			
+
+			distPath = pathfinder.findShortestPathBetween(start.getName(),
+					end.getName());
+			timePath = pathfinder.findFastestPath(start.getName(),
+					end.getName());
+
 			System.out.println("Distance path");
 			System.out.println(distPath);
 			System.out.println("Distance: " + distPath.getPathLength());
@@ -138,19 +145,11 @@ public class AStarTest
 			System.out.println(timePath);
 			System.out.println("Distance: " + timePath.getPathLength());
 			System.out.println("Time: " + timePath.getPathTravelTime());
-			
+
 			assertTrue(distPath.getPathLength() <= timePath.getPathLength());
-			assertTrue(distPath.getPathTravelTime() >= timePath.getPathTravelTime());
+			assertTrue(distPath.getPathTravelTime() >= timePath
+					.getPathTravelTime());
 		}
 	}
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
+
 }
